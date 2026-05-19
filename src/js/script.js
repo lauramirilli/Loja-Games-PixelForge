cbdb83588dbc5a4ef9e4b90c2e5f5ad35d38c498
lauramirilli <lcardinmirilli@gmail.com>
2026-05-19 14:35:19 -0300

    feat: adicionando header

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    const intro = document.getElementById("inicio");
+    const content = document.getElementById("seuElementoAqui"); // ← precisa disso
+    setTimeout(() => {
+        intro.classList.add("efeito-out");
+        setTimeout(() => {
+            intro.style.display = "none";
+            content.classList.remove("hidden");
+            content.classList.add("efeito-in");
+        }, 1000);
+    }, 3000);
+}
